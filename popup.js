53b2fcc0cc8cdc417a7058a05122d4f40aa4df19
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-03-20 20:35:38 +0530

    commited

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 
 let playPauseButton = document.getElementById('playPause');
-// let playPauseImage = document.getElementById('playPauseImg');
+let playPauseImage = document.getElementById('playPauseImg');
 let isPlaying = false;
 
 function togglePlayPause() {
   if (isPlaying) {
     playPauseButton.textContent = 'Play';
-    // playPauseImage.src = 'play.jpg';
+    playPauseImage.src = 'play.jpg';
   } else {
     playPauseButton.textContent = 'Pause';
-    // playPauseImage.src = 'pause.jpeg';
+    playPauseImage.src = 'pause.jpeg';
   }
   isPlaying = !isPlaying;
 }
