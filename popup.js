9ea8eaace762c5a1ad0b320822e166823601f762
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-03-22 20:30:21 +0530

    commited

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 
 let playPauseButton = document.getElementById('playPause');
 let playPauseImage = document.getElementById('playPauseImg');
-let isPlaying = false;
 
+let isPlaying = false;
 function togglePlayPause() {
   if (isPlaying) {
     // playPauseButton.textContent = 'Play';
@@ -13,5 +13,78 @@ function togglePlayPause() {
   }
   isPlaying = !isPlaying;
 }
-
 playPauseButton.addEventListener('click', togglePlayPause);
+
+
+
+
+
+
+
+
+
+
+// let audio = new Audio();
+
+// let isPlayingAudio = false;
+// function playAudio() {
+//     if (!isPlayingAudio) {
+//       audio.play();
+//     } else {
+//       audio.pause();
+//     }
+//     isPlayingAudio = !isPlayingAudio;
+//   }
+//   playPauseButton.addEventListener('click', playAudio);
+
+
+
+
+  
+let audioContext = new AudioContext();
+
+// let isPlayingAudioContext = false;
+// function playAudioContext() {
+//     if (!isPlayingAudioContext) {
+//         audioContext.play();
+//     } else {
+//         audioContext.pause();
+//     }
+//     isPlayingAudioContext = !isPlayingAudioContext;
+//   }
+//   playPauseButton.addEventListener('click', playAudioContext);
+
+// let baseFreq = document.getElementById('baseFre').value;
+// let beatFreq = document.getElementById('beatsFre').value;
+// let vol = document.getElementById('volume').value;
+
+// let leftEarOsc = audioContext.createOscillator();
+// let rightEarOsc = audioContext.createOscillator();
+
+// leftEarOsc.type = "sine";
+// rightEarOsc.type = "sine";
+
+// leftEarOsc.frequency.value = Math.round((baseFreq+beatFreq)/5);
+// rightEarOsc.frequency.value = Math.round((baseFreq-beatFreq)/5);
+
+// let gainNode = audioContext.createGain();
+// gainNode.gain.value = vol;
+
+// leftEarOsc.connect(gainNode);
+// rightEarOsc.connect(gainNode);
+
+// gainNode.connect(audioContext.destination);
+
+
+// let isPlayingMusic = false;
+// function playMusic() {
+//     if (!isPlayingMusic) {
+//         leftEarOsc.start();
+//         rightEarOsc.start();
+//     } else {
+//         leftEarOsc.stop();
+//         rightEarOsc.stop();
+//     }
+//     isPlayingMusic = !isPlayingMusic;
+//   }
+//   playPauseButton.addEventListener('click', playMusic);
