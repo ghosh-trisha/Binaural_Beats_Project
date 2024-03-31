89a836f9e5f9d7278d8beea34ff21da06ec88146
ghosh-trisha <trisha.ghosh1620@gmail.com>
2024-03-31 09:15:03 +0530

    commited

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// function for opening a new medetation tab at 1st means at 0th  index means tabs[0]
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === "openNewTab") {
       chrome.tabs.create({ url: message.url , index: 0});
